2a13bb8c91abe5eb4ee62e754db72caab8c39357
Pickler Projects <121974427+PicklerPaste@users.noreply.github.com>
2023-03-11 11:37:22 -0300

    Add files via upload

---
+++ JavaScript - Consumindo API + Array/app/main.js
@@ -0,0 +1,11 @@
+let livros = [];
+const endpointdaAPI = "https://guilhermeonrails.github.io/casadocodigo/livros.json";
+getBuscarLivrosDaAPI();
+
+async function getBuscarLivrosDaAPI() {
+    const res = await fetch(endpointdaAPI);
+
+    livros = await res.json();
+    let livrosComDesconto = aplicarDesconto(livros);
+    exibirOsLivrosNaTela(livrosComDesconto);
+}
